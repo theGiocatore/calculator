36f0fedb08c7e771aa91133b12b1b65e0e596ecb
theGiocatore <djordjemarcetic@gmail.com>
2020-07-24 15:01:41 +0200

    basic js calculator

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+function calcLogic() {
+
+    document.querySelectorAll('.vrednost').forEach(item => {
+        item.addEventListener('click', myFunction => {
+            var unos = item.innerHTML;
+            var broj = document.getElementById("ekran").innerHTML;
+
+            if (Number(unos) >= 0) {
+                var broj = document.getElementById("ekran").innerHTML = (String(broj) + String(unos));
+            }
+            else if (unos == "RESET") {
+                (document.getElementById("ekran")).innerHTML = null
+            }
+            else if (item.innerHTML == "C") {
+                var broj2 = document.getElementById("ekran").innerHTML;
+                var noviBroj = broj2.slice(0, -1);
+                document.getElementById("ekran").innerHTML = noviBroj;
+                var broj = noviBroj;
+            }
+            else if (item.innerHTML == "+" || item.innerHTML == "-" || item.innerHTML == "/" || item.innerHTML == "*") {
+                var broj2 = Number(document.getElementById("ekran").innerHTML);
+                var broj = document.getElementById("ekran").innerHTML = (String(broj) + String(unos));
+            }
+            else if (item.innerHTML == "=") {
+                var broj0 = document.getElementById("ekran").innerHTML;
+                var noviBroj0 = eval(broj0);
+                document.getElementById("ekran").innerHTML = noviBroj0
+            }
+        });
+    })
+
+}
